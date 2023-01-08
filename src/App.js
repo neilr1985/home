dae26db701d9405c8f4972cf0e08e805db75bda0
neilr1985 <neilr1985@hotmail.co.uk>
2023-01-08 16:43:00 +0000

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './styling/css.css';
+import neil from './assets/neilr.png';
+import LinkedInLogo from "./assets/linkedin-logo.svg";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+      <h1>Neil Robertson</h1>
+        <img id="image"  src={neil} className="Photo" alt="logo" />
+        <ul id="social-media-icons">
+        <li>
+          <a
+            href="https://uk.linkedin.com/in/neil-robertson-83811762"
+            target="_blank"
+            ><img class="icon" src={LinkedInLogo}
+          /></a>
+        </li>
+      </ul>
+      <p>Software engineer from Edinburgh, working in London 
+          </p>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/> 
+      </div>
     </div>
   );
 }
